9d906c5d08a1da57f8b7df5450fe89d31a50f72f
Lyquang <lythanhnhatquangthongnhat2004@gmail.com>
2025-08-26 17:06:13 +0700

    first be commit

---
+++ src/main/java/com/gucardev/backend/model/BaseModel.java
@@ -0,0 +1,44 @@
+//package com.gucardev.backend.model;
+//
+//import lombok.Data;
+//import org.hibernate.annotations.CreationTimestamp;
+//
+//import javax.persistence.*;
+//import java.util.Date;
+//
+//@MappedSuperclass
+//@Data
+//public abstract class BaseModel {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(nullable = false, updatable = false)
+//    @CreationTimestamp
+//    private Date createdDateTime;
+//
+//
+//}
+
+package com.gucardev.backend.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.*;   // 🔥 dùng jakarta thay vì javax
+import java.util.Date;
+
+@MappedSuperclass
+@Data
+public abstract class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Date createdDateTime;
+}
+
